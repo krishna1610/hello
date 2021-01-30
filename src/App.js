8a62588c40da4a8e8a65bf458bcb22e2b164bde8
Krishna <krishna@Krishnas-MacBook-Pro.local>
2021-01-29 16:03:45 -0800

    addimage

--- src/App.js
+++ src/App.js
@@ -8,6 +8,10 @@ function App() {
         Science field. I have 3 years of exprience in building Websites using technologies like HTML5, CSS3, JavaScript,
         React and Redux. I am looking for Front End Developer roles. 
       </p>
+      <img src="https://s3-us-west-1.amazonaws.com/files.klar.works/Krishna/profile.jpeg"></img>
+      <img src="https://s3-us-west-1.amazonaws.com/files.klar.works/Krishna/cake.JPG"></img>
+      <img src="https://s3-us-west-1.amazonaws.com/files.klar.works/Krishna/birthday.JPG"></img>
+      
     </div>
   );
 }
